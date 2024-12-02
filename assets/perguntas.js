203923e525080c9882c46c300b6cfda13c5589e6
maferferreira <mafe10232007110208@gmail.com>
2024-12-02 14:32:41 -0300

    fim

---
+++ assets/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'matematica',
+    'qual é a raiz quadrada de 100?',
+    '100'
+)
+
+criaCartao(
+    'sociologia',
+    'o que é racismo estrutural',
+    'O racismo estrutural é um tipo de racismo que está presente na estrutura da sociedade. Ou seja, é uma discriminação social que está enraizada na sociedade'
+)
+
+criaCartao(
+    'portugues',
+    'uma caracteristica da primeira fase do romantismo',
+    'nacionalista'
+)
+
+criaCartao(
+    'historia',
+    'qual o nome do presidente do brasil que ficou conhecido como Jango',
+    'João Goulart'
+)
+
+criaCartao(
+    'socioloia',
+    'o que é direiros humanos',
+    'normas que protegem a dignidade e os direitos basicos de um ser humano'
+)
